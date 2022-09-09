a2040e9227d1e3cb68d86f86ad4eab4eb921c833
Kondalov Ivan <kondalovi@gmail.com>
2022-09-09 13:20:23 +0400

    array exercises done

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,16 +1,29 @@
-const root = document.querySelector(":root");
-const colorPicker = document.querySelector("#base");
-const blurSlider = document.querySelector("#blur");
-const spacing = document.querySelector("#spacing");
+// const root = document.querySelector(":root");
+// const colorPicker = document.querySelector("#base");
+// const blurSlider = document.querySelector("#blur");
+// const spacing = document.querySelector("#spacing");
 
-colorPicker.addEventListener("change", () => {
-	root.style.setProperty("--color", colorPicker.value);
-});
+// colorPicker.addEventListener("change", () => {
+// 	root.style.setProperty("--color", colorPicker.value);
+// });
 
-blurSlider.addEventListener("change", () => {
-	root.style.setProperty("--blur", `${blurSlider.value}px`);
-});
+// blurSlider.addEventListener("change", () => {
+// 	root.style.setProperty("--blur", `${blurSlider.value}px`);
+// });
 
-spacing.addEventListener("change", () => {
-	root.style.setProperty("--padding", `${spacing.value}px`);
-});
+// spacing.addEventListener("change", () => {
+// 	root.style.setProperty("--padding", `${spacing.value}px`);
+// });
+
+const inputs = document.querySelectorAll("input");
+
+function change() {
+	const suffix = this.dataset.sizing || "";
+	document.documentElement.style.setProperty(
+		`--${this.name}`,
+		this.value + suffix
+	);
+}
+
+inputs.forEach((input) => input.addEventListener("change", change));
+inputs.forEach((input) => input.addEventListener("mousemove", change));
